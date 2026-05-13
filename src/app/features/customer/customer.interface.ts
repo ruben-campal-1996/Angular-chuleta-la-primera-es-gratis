a882ca549ff29d2bcd4b84a9afb32af05ebfa993
Rubén Campal <ruben.campal.lopez1996@gmail.com>
2026-05-13 09:14:52 +0200

    add interface for costumer

---
+++ src/app/features/customer/customer.interface.ts
@@ -0,0 +1,4 @@
+export interface ICustomer {
+    name:string,
+    avatar:string
+}
--- src/app/features/customer/customer.ts
+++ src/app/features/customer/customer.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './customer.css',
 })
 export class Customer {
-
+  customers = [
+    { name: 'Juan García', avatar: 'https://i.pravatar.cc/150?u=juangarcia' },
+    { name: 'María López', avatar: 'https://i.pravatar.cc/150?u=marialopez' },
+    { name: 'Carlos Rodríguez', avatar: 'https://i.pravatar.cc/150?u=carlosrodriguez' },
+    { name: 'Ana Martínez', avatar: 'https://i.pravatar.cc/150?u=anamartinez' },
+    { name: 'Pedro Sánchez', avatar: 'https://i.pravatar.cc/150?u=pedrosanchez' },
+    { name: 'Laura Fernández', avatar: 'https://i.pravatar.cc/150?u=laurafernandez' },
+    { name: 'Diego Moreno', avatar: 'https://i.pravatar.cc/150?u=diegomoreno' },
+    { name: 'Elena Jiménez', avatar: 'https://i.pravatar.cc/150?u=elenajimenez' },
+    { name: 'Miguel Torres', avatar: 'https://i.pravatar.cc/150?u=migueltorres' },
+    { name: 'Sofia Ruiz', avatar: 'https://i.pravatar.cc/150?u=sofiaruiz' }
+  ];
 }
+
